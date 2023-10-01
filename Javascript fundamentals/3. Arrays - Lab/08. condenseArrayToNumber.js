a5608428d09a8a56a3d61aa2ba7062f57fcd4eb5
IvayloTashev <i.tashev@abv.bg>
2023-10-01 08:55:17 +0300

    Added exercise 8 from Arrays Lab

--- Javascript fundamentals/3. Arrays - Lab/08. condenseArrayToNumber.js
+++ Javascript fundamentals/3. Arrays - Lab/08. condenseArrayToNumber.js
@@ -1,6 +1,20 @@
-function condenseArrayToNumber(arr) {
+function condenseArrayToNumber(nums) {
 
+    let condensed = [];
+
+    while (nums.length !== 1) {
+
+        for (let i = 0; i < nums.length - 1; i++) {
+
+            condensed[i] = nums[i] + nums[i + 1];
+        }
+
+        nums = condensed;
+        condensed = [];
+    }
+
+    console.log(nums.join());
 
 }
 
-condenseArrayToNumber([2,10,3])
+condenseArrayToNumber([5])
