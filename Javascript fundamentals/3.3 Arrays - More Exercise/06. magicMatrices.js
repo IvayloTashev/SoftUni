19a468f5d0067ec0f1a8bdc8c62882407a506590
IvayloTashev <i.tashev@abv.bg>
2023-10-04 18:10:22 +0300

    Added exercise 5 & 6 from Array - More Exercise

---
+++ Javascript fundamentals/3.3 Arrays - More Exercise/06. magicMatrices.js
@@ -0,0 +1,38 @@
+function magicMatrices(arr) {
+
+    let rowsArr = [];
+    let columnArr = [];
+    isMagic = true;
+
+
+    for (let i = 0; i < arr.length; i++) {
+
+        let rowSum = 0;
+        let columnSum = 0;
+
+        for (let j = 0; j < arr.length; j++) {
+
+            rowSum += arr[i][j];
+            columnSum += arr[j][i];
+        }
+
+        rowsArr.push(rowSum)
+        columnArr.push(columnSum)
+    }
+
+    for (let k = 0; k < rowsArr.length; k++) {
+    
+        if (rowsArr [k] !== columnArr[k]){
+            isMagic = false;
+            break;
+        }
+
+    }
+
+    console.log(isMagic);
+
+}
+
+magicMatrices([[4, 5, 6],
+[6, 5, 4],
+[5, 5, 5]])
