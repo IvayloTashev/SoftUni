bb11b2362704670764f0cd211eb015726734add4
IvayloTashev <i.tashev@abv.bg>
2024-03-31 12:06:48 +0300

    update like functionality

--- Javascript Applications/07. Exam Preparation/04. Fun Facts/src/data/facts.js
+++ Javascript Applications/07. Exam Preparation/04. Fun Facts/src/data/facts.js
@@ -4,6 +4,9 @@ const endpoints = {
     allfacts: "/data/facts?sortBy=_createdOn%20desc",
     factById: "/data/facts/",
     facts: "/data/facts",
+    like: "/data/likes",
+    count: (factId) => `/data/likes?where=factId%3D%22${factId}%22&distinct=_ownerId&count`,
+    likedAlready: (factId, userId) => `/data/likes?where=factId%3D%22${factId}%22%20and%20_ownerId%3D%22${userId}%22&count`
 };
 
 async function getAllFacts() {
@@ -36,10 +39,25 @@ async function deleteFact(id) {
     return del(endpoints.factById + id);
 };
 
+async function giveLike(factId) {
+    await post(endpoints.like, {factId})
+};
+
+async function checklikes(factId) {
+    return get(endpoints.count(factId))
+};
+
+async function userAlreadyLike(factId, userId) {
+    return get(endpoints.likedAlready(factId, userId))
+};
+
 export {
     getAllFacts,
     getFactById,
     updateFact,
     createFact,
-    deleteFact
+    deleteFact,
+    giveLike,
+    checklikes,
+    userAlreadyLike
 }
--- Javascript Applications/07. Exam Preparation/04. Fun Facts/src/views/details.js
+++ Javascript Applications/07. Exam Preparation/04. Fun Facts/src/views/details.js
@@ -1,8 +1,8 @@
-import { getFactById } from "../data/facts.js";
+import { checklikes, getFactById, giveLike, userAlreadyLike } from "../data/facts.js";
 import { getUserData } from "../data/util.js";
 import { html, render, page } from "../lib.js";
 
-const detailsTemp = (factData, ownerId) => html`
+const detailsTemp = (factData, likes, alreadyLiked, hasUser, isOwner) => html`
 <section id="details">
     <div id="details-wrapper">
         <img id="details-img" src=${factData.imageUrl} alt="example1" />
@@ -13,22 +13,50 @@ const detailsTemp = (factData, ownerId) => html`
                 <p id ="more-info">${factData.moreInfo}</p>
             </div>
 
-            <h3>Likes:<span id="likes">0</span></h3>
+            <h3>Likes:<span id="likes">${likes}</span></h3>
 
-            ${ownerId == factData._ownerId ? html`
+            ${hasUser ? html`
             <div id="action-buttons">
+                ${isOwner ? html`
                 <a href="/edit/${factData._id}" id="edit-btn">Edit</a>
-                <a href="/delete/${factData._id}" id="delete-btn">Delete</a>
-
-                <a href="" id="like-btn">Like</a>
-            </div>` : ""}
+                <a href="/delete/${factData._id}" id="delete-btn">Delete</a>` : (!alreadyLiked ? html`
+                <a href="javascript:void(0)" id="like-btn" @click=${onLike}>Like</a>` : null)}
+            </div>` : null}
         </div>
     </div>
 </section>`;
 
+let context = null;
 export async function showDetails(ctx) {
-    const id = ctx.params.id;
-    const  ownerId = getUserData()?._id;
-    const factData = await getFactById(id);
-    render(detailsTemp(factData, ownerId))
+    context = ctx;
+    const id = context.params.id;
+
+    const requests = [
+        getFactById(id),
+        checklikes(id)
+    ];
+
+    const user = getUserData();
+    
+    if (user) {
+        requests.push(userAlreadyLike(id, user._id))
+    };
+  
+    const [factData, likes, alreadyLiked] = await Promise.all(requests);
+
+    const hasUser = !!user;
+    const isOwner = hasUser && factData._ownerId == user._id;
+
+
+    render(detailsTemp(factData, likes, alreadyLiked, hasUser, isOwner))
+}
+
+async function onLike(event) {
+    event.preventDefault();
+    const id = context.params.id;
+
+    await giveLike(id);
+
+    page.redirect(`/details/${id}`)
+
 }
