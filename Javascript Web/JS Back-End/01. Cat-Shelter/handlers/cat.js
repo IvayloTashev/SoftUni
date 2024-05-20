40bea226719a40bb94a5b11e1331db9d3a2517dc
IvayloTashev <i.tashev@abv.bg>
2024-05-20 21:21:09 +0300

    Update Cat-Shelter

--- Javascript Web/JS Back-End/01. Cat-Shelter/handlers/cat.js
+++ Javascript Web/JS Back-End/01. Cat-Shelter/handlers/cat.js
@@ -10,7 +10,6 @@ module.exports = (req, res) => {
     const pathname = url.parse(req.url).pathname;
 
     if (pathname === "/cats/add-cat" && req.method === "GET") {
-
         let filePath = path.normalize(path.join(__dirname, "../views/addCat.html"));
 
         const readStream = fs.createReadStream(filePath);
@@ -30,7 +29,6 @@ module.exports = (req, res) => {
         });
 
     } else if (pathname === "/cats/add-breed" && req.method === "GET") {
-
         let filePath = path.normalize(path.join(__dirname, "../views/addBreed.html"));
 
         const readStream = fs.createReadStream(filePath);
@@ -51,52 +49,32 @@ module.exports = (req, res) => {
 
     } else if (pathname === "/cats/add-cat" && req.method === "POST") {
         let formData = "";
-        res.on("data", (data) => {
+
+        req.on("data", (data) => {
             formData += data;
         });
 
-        let body = qs.parse(formData);
+        req.on("end", () => {
+            let body = qs.parse(formData);
+            
+            fs.readFile(`./data/cats.json`, (err, data) => {
+                if (err) {
+                    throw err;
+                };
+                
+                let cats = JSON.parse(data);
+                cats.push(body.cat);
+                let json = JSON.stringify(cats);
+                
+                fs.writeFile(`../data/cats.json`, json, `utf-8`, () => console.log("The cat was added succsesfully"));
+            });
 
-        fs.readFile(`./data/cats.json`, (err, data) => {
-            if (err) {
-                throw err;
-            };
-
-            let cats = JSON.parse(data);
-            cats.push(body.cat);
-
-            let json = JSON.stringify(cats);
-
-            fs.writeFile(`../data/cats.json`, json, `utf-8`, () => console.log("The cat was added succsesfully"));
+            res.writeHead(302, { Location: '/' });
+            req.end();
         });
-
-        res.writeHead(302, { Location: '/' });
-        req.end();
 
     } else if (pathname === "/cats/add-breed" && req.method === "POST") {
-        let formData = "";
-        res.on("data", (data) => {
-            formData += data;
-        });
-
-        let body = qs.parse(formData);
-
-        fs.readFile(`../data/breeds.json`, (err, data) => {
-            if (err) {
-                throw err;
-            };
-
-            let breeds = JSON.parse(data);
-            breeds.push(body.breed);
-
-            let json = JSON.stringify(breeds);
-
-            fs.writeFile('../data/breeds.json', json, `utf-8`, () => console.log("The breed was added succsesfully"));
-        });
-
-        res.writeHead(302, { Location: '/' });
-        req.end();
-
+        
     }
 
     else {
