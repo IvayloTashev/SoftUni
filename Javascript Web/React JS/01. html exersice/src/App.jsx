fb1b35d15646a417e8b42957923a334a6436c680
IvayloTashev <i.tashev@abv.bg>
2024-06-30 09:30:59 +0300

    Add react exercise

---
+++ Javascript Web/React JS/01. html exersice/src/App.jsx
@@ -0,0 +1,35 @@
+import Navigation from "./components/Navigation";
+import Banner from "./components/Banner";
+import About from "./components/About";
+import VideoSection from "./components/VideoSection";
+import Features from "./components/Features";
+import Team from "./components/Team";
+import Testimonial from "./components/Testimonial";
+import Faq from "./components/Faq";
+import Contact from "./components/Contact";
+import Download from "./components/Download";
+import { Footer } from "./components/Footer";
+
+
+function App() {
+
+  return (
+    <>
+
+      <Navigation />
+      <Banner />
+      <About />
+      <VideoSection />
+      <Features />
+      <Team />
+      <Testimonial />
+      <Faq />
+      <Contact />
+      <Download />
+      <Footer />
+      
+    </>
+  )
+}
+
+export default App
