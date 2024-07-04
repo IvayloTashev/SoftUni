e8af6a1d6f5d091b9780c195f4676a53ed2247f5
IvayloTashev <i.tashev@abv.bg>
2024-07-04 20:07:02 +0300

    Update

--- Javascript Web/React JS/01. html exersice/src/components/Features.jsx
+++ Javascript Web/React JS/01. html exersice/src/components/Features.jsx
@@ -1,3 +1,5 @@
+import FeaturesSpecs from "./FeaturesSpecs";
+
 export default function Features() {
     return (
         <section className="feature section-padding" data-scroll-index='2'>
@@ -12,53 +14,21 @@ export default function Features() {
                         <div className="section-content text-center">
                             <div className="row">
                                 <div className="col-md-4 col-sm-12">
-                                    <div className="media single-feature wow fadeInUp" data-wow-delay="0.2s">
-                                        <div className="media-body text-right media-right-margin">
-                                            <h5>Fast Processing</h5>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididugnt ut labore</p>
-                                        </div>
-                                        <div className="media-right icon-border"> <span className="fa fa-bolt" aria-hidden="true"></span> </div>
-                                    </div>
-                                    <div className="media single-feature wow fadeInUp" data-wow-delay="0.4s">
-                                        <div className="media-body text-right media-right-margin">
-                                            <h5>Low Power Consuming</h5>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididugnt ut labore</p>
-                                        </div>
-                                        <div className="media-right icon-border"> <span className="fa fa-battery-full" aria-hidden="true"></span> </div>
-                                    </div>
-                                    <div className="media single-feature wow fadeInUp" data-wow-delay="0.6s">
-                                        <div className="media-body text-right media-right-margin">
-                                            <h5>Wifi Compatibility</h5>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididugnt ut labore</p>
-                                        </div>
-                                        <div className="media-right icon-border"> <span className="fa fa-wifi" aria-hidden="true"></span> </div>
-                                    </div>
+
+                                    <FeaturesSpecs name={'Fast Processing'} className={'fa fa-bolt'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididugnt ut labore'}></FeaturesSpecs>
+                                    <FeaturesSpecs name={'Low Power Consuming'} className={'fa fa-battery-full'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididugnt ut labore'}></FeaturesSpecs>
+                                    <FeaturesSpecs name={'Wifi Compatibility'} className={'fa fa-wifi'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididugnt ut labore'}></FeaturesSpecs>
+
                                 </div>
                                 <div className="col-md-4 d-none d-md-block d-lg-block">
                                     <div className="feature-mobile"> <img src="images/iphone-screen-with-shadow.png" className="img-fluid wow fadeInUp" /> </div>
                                 </div>
                                 <div className="col-md-4 col-sm-12">
-                                    <div className="media single-feature wow fadeInUp" data-wow-delay="0.2s">
-                                        <div className="media-left icon-border media-right-margin"> <span className="fa fa-check-double" aria-hidden="true"></span> </div>
-                                        <div className="media-body text-left">
-                                            <h5>Regular Updates</h5>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididugnt ut labore</p>
-                                        </div>
-                                    </div>
-                                    <div className="media single-feature wow fadeInUp" data-wow-delay="0.4s">
-                                        <div className="media-left icon-border media-right-margin"> <span className="fa fa-dollar-sign" aria-hidden="true"></span> </div>
-                                        <div className="media-body text-left">
-                                            <h5>Save Money</h5>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididugnt ut labore</p>
-                                        </div>
-                                    </div>
-                                    <div className="media single-feature wow fadeInUp" data-wow-delay="0.6s">
-                                        <div className="media-left icon-border media-right-margin"> <span className="fa fa-hdd" aria-hidden="true"></span> </div>
-                                        <div className="media-body text-left">
-                                            <h5>Unlimited Storage</h5>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididugnt ut labore</p>
-                                        </div>
-                                    </div>
+
+                                    <FeaturesSpecs name={'Regular Updates'} className={'fa fa-check-double'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididugnt ut labore'}></FeaturesSpecs>
+                                    <FeaturesSpecs name={'Save Money'} className={'fa fa-dollar-sign'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididugnt ut labore'}></FeaturesSpecs>
+                                    <FeaturesSpecs name={'Unlimited Storage'} className={'fa fa-hdd'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididugnt ut labore'}></FeaturesSpecs>
+
                                 </div>
                             </div>
                         </div>
@@ -68,3 +38,5 @@ export default function Features() {
         </section>
     )
 }
+
+
--- Javascript Web/React JS/01. html exersice/src/components/FeaturesSpecs.jsx
+++ Javascript Web/React JS/01. html exersice/src/components/FeaturesSpecs.jsx
@@ -1,11 +1,12 @@
-export default function FeaturesSpecs() {
+export default function FeaturesSpecs(props) {
     return(
         <div className="media single-feature wow fadeInUp" data-wow-delay="0.2s">
         <div className="media-body text-right media-right-margin">
-            <h5>Fast Processing</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididugnt ut labore</p>
+            <h5>{props.name}</h5>
+            <p>{props.description}</p>
         </div>
-        <div className="media-right icon-border"> <span className="fa fa-bolt" aria-hidden="true"></span> </div>
+        <div className="media-right icon-border"> <span className={props.className} aria-hidden="true"></span> </div>
     </div>
     )
 }
+
