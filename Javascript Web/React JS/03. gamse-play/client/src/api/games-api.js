68837cc2b2db58c5194e8cdc153817ac5853e1be
IvayloTashev <i.tashev@abv.bg>
2024-07-26 21:31:01 +0300

    Add catalog page

---
+++ Javascript Web/React JS/03. gamse-play/client/src/api/games-api.js
@@ -0,0 +1,10 @@
+import * as requester from "./requester";
+
+const BASE_URL = 'http://localhost:3030/jsonstore/games'
+
+export const getAll = async () => {
+   const result = await requester.get(BASE_URL);
+   const games = Object.values(result);
+
+   return games;
+}
