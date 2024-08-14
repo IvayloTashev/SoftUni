e115e6cd928ea232602c632b086675122cc5dcdf
IvayloTashev <i.tashev@abv.bg>
2024-08-14 18:27:19 +0300

    add logout

--- Javascript Web/React JS/03. gamse-play/client/src/App.jsx
+++ Javascript Web/React JS/03. gamse-play/client/src/App.jsx
@@ -8,6 +8,7 @@ import GameList from "./components/game-list/GameList"
 import GameCreate from "./components/game-create/GameCreate"
 import GameDetails from "./components/game-details/GameDetails"
 import { AuthContextProvider } from "./contexts/AuthContext"
+import Logout from "./components/logout/Logout"
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/logout" element={<Logout />} />
             <Route path="/catalog" element={<GameList />} />
             <Route path="/games/:gameId" element={<GameDetails />} />
             <Route path="/create" element={<GameCreate />} />
