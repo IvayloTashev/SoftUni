1eb19939182095a103b7da4f886ff2dc9d7e6d81
IvayloTashev <i.tashev@abv.bg>
2024-11-12 17:54:13 +0200

    update the main folder architecture

--- JS Front-End/Angular/Workshop/post-blog/src/app/main/themes-list/themes-list.component.ts
+++ JS Front-End/Angular/Workshop/post-blog/src/app/main/themes-list/themes-list.component.ts
@@ -1,24 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../api.service';
-import { Theme } from '../types/theme';
+import { ApiService } from '../../api.service';
+import { Theme } from '../../types/theme';
+import { LoaderComponent } from '../../shared/loader/loader.component';
 
 @Component({
   selector: 'app-themes-list',
   standalone: true,
-  imports: [],
+  imports: [LoaderComponent],
   templateUrl: './themes-list.component.html',
   styleUrl: './themes-list.component.css'
 })
 export class ThemesListComponent implements OnInit {
 
   themes: Theme[] = [];
+  isLoading = true;
 
   constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
     this.apiService.getThemes().subscribe(themes => {
-      this.themes = themes
+      this.themes = themes;
+      this.isLoading = false;
     })
   }
 
