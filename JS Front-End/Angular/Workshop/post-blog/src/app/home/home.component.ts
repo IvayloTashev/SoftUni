97a1d51b74833fcbfb85b3ec7bf67a9c7391264e
IvayloTashev <i.tashev@abv.bg>
2024-11-22 18:59:05 +0200

    Refactoring home and main component

--- JS Front-End/Angular/Workshop/post-blog/src/app/home/home.component.ts
+++ JS Front-End/Angular/Workshop/post-blog/src/app/home/home.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { WelcomeMsgComponent } from '../shared/welcome-msg/welcome-msg.component';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, WelcomeMsgComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  get isLoggedIn(): boolean {
+    return this.userService.IsLogged;
+  }
+
+  constructor(private userService: UserService) {}
 
 }
--- JS Front-End/Angular/Workshop/post-blog/src/app/main/main.component.ts
+++ JS Front-End/Angular/Workshop/post-blog/src/app/main/main.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { ThemesListComponent } from '../themes/themes-list/themes-list.component';
 import { PostsListComponent } from '../posts/posts-list/posts-list.component';
+import { WelcomeMsgComponent } from "../shared/welcome-msg/welcome-msg.component";
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [ThemesListComponent, PostsListComponent],
+  imports: [ThemesListComponent, PostsListComponent, WelcomeMsgComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
+
+  get isLoggedIn(): boolean {
+    return this.userService.IsLogged;
+  }
+
+  constructor(private userService: UserService) {} 
 
 }
