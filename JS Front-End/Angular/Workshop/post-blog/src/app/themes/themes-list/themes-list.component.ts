79e61ec823bc28a78bec24bb5cd5d92da2252693
IvayloTashev <i.tashev@abv.bg>
2024-12-10 21:57:37 +0200

    Update post and theme list

--- JS Front-End/Angular/Workshop/post-blog/src/app/themes/themes-list/themes-list.component.ts
+++ JS Front-End/Angular/Workshop/post-blog/src/app/themes/themes-list/themes-list.component.ts
@@ -1,14 +1,15 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../api.service';
 import { Theme } from '../../types/theme';
 import { LoaderComponent } from '../../shared/loader/loader.component';
 import { RouterLink } from '@angular/router';
+import { SlicePipe } from '../../shared/pipes/slice.pipe';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-themes-list',
   standalone: true,
-  imports: [LoaderComponent, RouterLink],
+  imports: [LoaderComponent, RouterLink, SlicePipe, DatePipe],
   templateUrl: './themes-list.component.html',
   styleUrl: './themes-list.component.css'
 })
