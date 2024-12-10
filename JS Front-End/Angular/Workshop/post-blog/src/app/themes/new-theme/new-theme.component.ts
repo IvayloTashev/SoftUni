4031ac85b96ca8b7fee84216954fe5b46db09a54
IvayloTashev <i.tashev@abv.bg>
2024-12-10 21:57:14 +0200

    Update theme component

--- JS Front-End/Angular/Workshop/post-blog/src/app/themes/new-theme/new-theme.component.ts
+++ JS Front-End/Angular/Workshop/post-blog/src/app/themes/new-theme/new-theme.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../../api.service';
 import { FormsModule, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-theme',
@@ -11,16 +12,17 @@ import { FormsModule, NgForm } from '@angular/forms';
 })
 export class NewThemeComponent {
 
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService, private router: Router) { }
 
   addTheme(form: NgForm) {
     if(form.invalid) {
       return
     }
 
-    console.log(form.invalid);
-    console.log(form.value);
-    
-  }
+    const {themeName, postText} = form.value
 
+    this.apiService.createTheme(themeName, postText).subscribe(() => {
+      this.router.navigate(['/themes']);
+    })
+  }
 }
--- JS Front-End/Angular/Workshop/post-blog/src/app/themes/theme-content/theme-content.component.ts
+++ JS Front-End/Angular/Workshop/post-blog/src/app/themes/theme-content/theme-content.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Theme } from '../../types/theme';
 import { ApiService } from '../../api.service';
+import { UserService } from '../../user/user.service';
 
 @Component({
   selector: 'app-theme-content',
@@ -13,7 +14,15 @@ import { ApiService } from '../../api.service';
 export class ThemeContentComponent implements OnInit {
   theme = {} as Theme
 
-  constructor(private route: ActivatedRoute, private apiService: ApiService) { }
+  constructor(private route: ActivatedRoute, private apiService: ApiService, private userService: UserService) { }
+
+  get isLoggedIn(): boolean {
+    return this.userService.IsLogged;
+  }
+
+  get username():string {
+    return this.userService.user?.username || '';
+  }
 
   ngOnInit(): void {
     const id = this.route.snapshot.params['themeId'];
