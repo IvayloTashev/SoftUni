76449099bf011443f97f55b098238b9bc16b554c
IvayloTashev <i.tashev@abv.bg>
2025-04-27 19:01:00 +0300

    Add 3 codewars solutions

---
+++ Codewars/56. breakCamelCase.js
@@ -0,0 +1,27 @@
+/*
+Complete the solution so that the function will break up camel casing, using a space between words.
+Example
+
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+*/
+
+function solution(string) {
+    let res = '';
+
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] == string[i].toUpperCase()) {
+            res += ` ${string[i]}`
+        } else {
+            res += string[i]
+        }
+    }
+
+    return res;
+
+}
+
+solution('camelCasing');
+solution('identifier');
+solution('');
