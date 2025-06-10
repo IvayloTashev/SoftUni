5e6c4046081f0f593eacadda89874760e0e2b9c3
IvayloTashev <i.tashev@abv.bg>
2025-06-10 20:59:54 +0300

    Add task 3 and 4 from TypeScript Decorators

---
+++ TypeScript/05. TypeScript Decorators/03. createdOn.ts
@@ -0,0 +1,26 @@
+
+function createdAt(constructor: { new(...args: any[]): User }) {
+    return class extends constructor {
+        createdOn: Date = new Date();
+    }
+}
+
+@createdAt
+class User {
+    name: string
+    age: number
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+
+    displayUserInfo() {
+        console.log(`${this.name} Age: ${this.age}`);
+    }
+}
+
+const user1 = new User("John Doe", 30);
+user1.displayUserInfo()
+console.log(user1);
+console.log((user1 as any).createdOn);
