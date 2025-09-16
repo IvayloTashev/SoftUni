7230a0e05863c373ffe75d8e7fd156685201f17e
IvayloTashev <i.tashev@abv.bg>
2025-09-16 21:07:09 +0300

    Add task 13-25 from Basic CRUD

--- PostgreSQL/2. Basic CRUD/13. Missing Values.sql
+++ PostgreSQL/2. Basic CRUD/13. Missing Values.sql
@@ -1,12 +1,13 @@
-INSERT INTO 
-    departments(department, manager_id)
+SELECT
+	id,
+	first_name, 
+	last_name
+	
+FROM
+	employees
 
-VALUES 
-    ('Finance', 3),
-	('Information Services', 42),
-	('Document Control', 90),
-	('Quality Assurance', 274),
-	('Facilities and Maintenance', 218),
-	('Shipping and Receiving', 85),
-	('Executive', 109);
+WHERE 
+	middle_name IS NULL
+
+LIMIT 3
 ;
