3db2572aed7175198a279a5d803c2ed23429517f
IvayloTashev <i.tashev@abv.bg>
2025-10-01 20:40:08 +0300

    Add tasks 11-16 from Subqueries and Joins

---
+++ PostgreSQL/6. Subqueries and Joins/11. Bulgaria's Peaks Higher than 2835 Meters.sql
@@ -0,0 +1,21 @@
+SELECT
+	mc.country_code,
+	m.mountain_range,
+	p.peak_name,
+	p.elevation
+FROM
+	mountains AS m
+JOIN
+	peaks AS p
+ON
+	m.id = p.mountain_id
+JOIN
+	mountains_countries AS mc
+ON
+	m.id = mc.mountain_id
+WHERE
+	elevation > 2835
+	AND
+	country_code = 'BG'
+ORDER BY
+	elevation DESC;
