0bf2ad3a8394ca7d0e6f8c249e7488d84699b272
IvayloTashev <i.tashev@abv.bg>
2025-10-08 21:46:41 +0300

    Add Exam Preparation 2

---
+++ PostgreSQL/9. Exam Preparation 2/12. Animals with Owner or Not.sql
@@ -0,0 +1,25 @@
+CREATE OR REPLACE PROCEDURE sp_animals_with_owners_or_not(
+	IN animal_name VARCHAR(30),
+	OUT result VARCHAR(30)
+)
+AS
+$$
+	BEGIN
+		SELECT
+			o.name
+		FROM
+			animals AS a
+		JOIN
+			owners AS o
+		ON
+			a.owner_id = o.id
+		WHERE
+			a.name = animal_name
+		INTO result;
+
+		IF (result IS NULL) THEN
+			 result := 'For adoption';
+		END IF;
+	END;
+$$
+LANGUAGE plpgsql;
